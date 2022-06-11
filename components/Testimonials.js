30a031c81b8f65481af771aa4203fbfa042235bd
Ali <oioimolloy@gmail.com>
2022-06-11 17:38:35 +0100

    first build

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -14,10 +14,10 @@ export default function Testimonials({}) {
                 What Our Clients Say
               </h2>
               <p className="mb-6 text-lg leading-relaxed text-body-color sm:text-xl sm:leading-relaxed">
-                “I’ve been working with Archetype for years now, the quality of
-                their frames and plinths are excellent, they always have great
-                advice and has been able to accommodate some very tight
-                deadlines too, which is much appreciated!"
+                &ldquo;I’ve been working with Archetype for years now, the
+                quality of their frames and plinths are excellent, they always
+                have great advice and has been able to accommodate some very
+                tight deadlines too, which is much appreciated!&rdquo;.
               </p>
               <span className="text-sm font-semibold">Rachel Howard</span>
               <p className="text-xs text-[#969696]">Artist</p>
@@ -33,17 +33,17 @@ export default function Testimonials({}) {
             >
               <div className="mb-6 ud-testimonial-content">
                 <p className="text-base tracking-wide text-body-color">
-                  "Archetype produces bespoke conservation grade mounting and
-                  framing at the highest standard. They are a pleasure to work
-                  with, adapting to a wide range of briefs and offering
-                  invaluable advice."
+                  &ldquo;Archetype produces bespoke conservation grade mounting
+                  and framing at the highest standard. They are a pleasure to
+                  work with, adapting to a wide range of briefs and offering
+                  invaluable advice.&rdquo;
                 </p>
               </div>
               <div className="flex items-center ud-testimonial-info">
                 <div className="ud-testimonial-meta">
                   <span className="text-sm font-semibold">Simon Philpott</span>
                   <p className="text-xs text-[#969696]">
-                    Senior Paper Conservation Technician at Tate
+                    Senior Paper Conservation Technician at The Tate
                   </p>
                 </div>
               </div>
@@ -57,14 +57,20 @@ export default function Testimonials({}) {
             >
               <div className="mb-6 ud-testimonial-content">
                 <p className="text-base tracking-wide text-body-color">
-                  “We're impressed. It's all intuitive. It's clean. It's
-                  masterfully created. Craftsmanship."
+                  &ldquo;We started working with Archetype Fine Art Services a
+                  year ago. One experience was enough to trust them with all our
+                  orders since then. They handle everything so professionally,
+                  with a great level of care and precision. No matter how big
+                  the challenge, they always try their best to come up with the
+                  best solution!&rdquo;
                 </p>
               </div>
               <div className="flex items-center ud-testimonial-info">
                 <div className="ud-testimonial-meta">
-                  <span className="text-sm font-semibold">Margin Gesmu</span>
-                  <p className="text-xs text-[#969696]">Something Gallery</p>
+                  <span className="text-sm font-semibold">Azadeh Zaferani</span>
+                  <p className="text-xs text-[#969696]">
+                    Director, Ab-Anbar Gallery
+                  </p>
                 </div>
               </div>
             </div>
@@ -77,14 +83,18 @@ export default function Testimonials({}) {
             >
               <div className="mb-6 ud-testimonial-content">
                 <p className="text-base tracking-wide text-body-color">
-                  “Our members are so impressed. Thier work is inuative and
-                  clean. It's distraction free. If you're building a community.
+                  &ldquo;Lily is a patient, innovative and expert framer.
+                  Working with her is always fun; her keen eye and deep material
+                  knowledge have led to bespoke frame finishes and unique
+                  options as well as problem solving. I trust her judgment and
+                  expertise and her fresh take on framing is always a total
+                  pleasure to collaborate with.&rdquo;
                 </p>
               </div>
               <div className="flex items-center ud-testimonial-info">
                 <div className="ud-testimonial-meta">
-                  <span className="text-sm font-semibold">William Smith</span>
-                  <p className="text-xs text-[#969696]">Owner at Aspex</p>
+                  <span className="text-sm font-semibold">Emma Cousin</span>
+                  <p className="text-xs text-[#969696]">Artist</p>
                 </div>
               </div>
             </div>
@@ -92,80 +102,78 @@ export default function Testimonials({}) {
         </div>
         <div className="flex flex-wrap">
           <div className="w-full mx-4">
-            <div className="wow fadeInUp" data-wow-delay=".2s">
-              <div className="mb-8 text-center ud-title">
-                <h6 className="relative inline-flex items-center text-xs font-normal text-body-color">
-                  <span className="mr-4 inline-block h-[1px] w-8 bg-[#afb2b5] "></span>
-                  Some Of Our Clients
-                  <span className="ml-4 inline-block h-[1px] w-8 bg-[#afb2b5]"></span>
-                </h6>
+            <div className="mb-8 text-center ud-title">
+              <h6 className="relative inline-flex items-center text-xs font-normal text-body-color">
+                <span className="mr-4 inline-block h-[1px] w-8 bg-[#afb2b5] "></span>
+                Some Of Our Clients
+                <span className="ml-4 inline-block h-[1px] w-8 bg-[#afb2b5]"></span>
+              </h6>
+            </div>
+            <div className="flex flex-wrap ud-brands-logo place-content-center">
+              <div className="ud-single-logo mr-10 mb-5 max-w-[140px]">
+                <a
+                  href="https://www.sidmotiongallery.co.uk/"
+                  target="_blank"
+                  rel="nofollow noopner noreferrer"
+                >
+                  <img
+                    src="/images/clients/tate-logo.png"
+                    alt="tailgrids"
+                    className="duration-300 grayscale hover:filter-none"
+                  />
+                </a>
               </div>
-              <div className="flex flex-wrap ud-brands-logo place-content-center">
-                <div className="ud-single-logo mr-10 mb-5 max-w-[140px]">
-                  <a
-                    href="https://www.sidmotiongallery.co.uk/"
-                    target="_blank"
-                    rel="nofollow noopner"
-                  >
-                    <img
-                      src="/images/clients/tate-logo.png"
-                      alt="tailgrids"
-                      className="duration-300 grayscale hover:filter-none"
-                    />
-                  </a>
-                </div>
-                <div className="ud-single-logo mr-10 mb-5 max-w-[140px]">
-                  <a
-                    href="https://museum.wales/"
-                    target="_blank"
-                    rel="nofollow noopner"
-                  >
-                    <img
-                      src="/images/clients/nmw-logo.gif"
-                      alt="Museum of Wales"
-                      className="duration-300 grayscale hover:filter-none"
-                    />
-                  </a>
-                </div>
-                <div className="ud-single-logo mr-10 mb-5 max-w-[140px]">
-                  <a
-                    href="https://williammorrissociety.org/"
-                    target="_blank"
-                    rel="nofollow noopner"
-                  >
-                    <img
-                      src="/images/clients/williammorris-logo.jpg"
-                      alt="William Morris Society"
-                      className="duration-300 grayscale hover:filter-none"
-                    />
-                  </a>
-                </div>
-                <div className="ud-single-logo mr-10 mb-5 max-w-[140px]">
-                  <a
-                    href="https://graygrids.com"
-                    target="_blank"
-                    rel="nofollow noopner"
-                  >
-                    <img
-                      src="/images/clients/zardi-logo.png"
-                      alt="Zardi and Zardi"
-                      className="duration-300 grayscale hover:filter-none"
-                    />
-                  </a>
-                </div>
-                <div className="ud-single-logo mr-10 mb-5 max-w-[140px]">
-                  <a
-                    href="https://www.pangolinlondon.com/"
-                    target="_blank"
-                    rel="nofollow noopner"
-                  >
-                    <img
-                      src=""
-                      alt="Panagolin"
-                      className="duration-300 grayscale hover:filter-none"
-                    />
-                  </a>
-                </div>
+              <div className="ud-single-logo mr-10 mb-5 max-w-[140px]">
+                <a
+                  href="https://museum.wales/"
+                  target="_blank"
+                  rel="nofollow noopner noreferrer"
+                >
+                  <img
+                    src="/images/clients/nmw-logo.gif"
+                    alt="Museum of Wales"
+                    className="duration-300 grayscale hover:filter-none"
+                  />
+                </a>
+              </div>
+              <div className="ud-single-logo mr-10 mb-5 max-w-[140px]">
+                <a
+                  href="https://williammorrissociety.org/"
+                  target="_blank"
+                  rel="nofollow noopner noreferrer"
+                >
+                  <img
+                    src="/images/clients/williammorris-logo.jpg"
+                    alt="William Morris Society"
+                    className="duration-300 grayscale hover:filter-none"
+                  />
+                </a>
+              </div>
+              <div className="ud-single-logo mr-10 mb-5 max-w-[140px]">
+                <a
+                  href="https://graygrids.com"
+                  target="_blank"
+                  rel="nofollow noopner noreferrer"
+                >
+                  <img
+                    src="/images/clients/zardi-logo.png"
+                    alt="Zardi and Zardi"
+                    className="duration-300 grayscale hover:filter-none"
+                  />
+                </a>
+              </div>
+              <div className="ud-single-logo mr-10 mb-5 max-w-[140px]">
+                <a
+                  href="https://www.pangolinlondon.com/"
+                  target="_blank"
+                  rel="nofollow noopner noreferrer"
+                >
+                  <img
+                    src=""
+                    alt="Panagolin"
+                    className="duration-300 grayscale hover:filter-none"
+                  />
+                </a>
               </div>
             </div>
           </div>
